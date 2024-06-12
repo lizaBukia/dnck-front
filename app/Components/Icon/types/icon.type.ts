f1b1ef75c7c2f6029a39bacfc4672fd5f5123c6d
Gvanca <gvancanando@gmail.com>
2024-06-12 20:55:59 +0400

    chore: reusable icons

--- app/Components/Icon/types/icon.type.ts
+++ app/Components/Icon/types/icon.type.ts
@@ -1,5 +1,3 @@
-import {} from "next/script";
+import { IconPropsInterface } from '../interfaces/icon-props.interface';
 
-import {IconPropsInterface } from "../interfaces/icon-props.interface";
-
-export type IconType = (props: IconPropsInterface) => JSX.Element
+export type IconType = (props: IconPropsInterface) => JSX.Element;
