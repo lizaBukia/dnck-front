f14f6d2fd3af7e80e01b4bd1b168f7ab5a51c2f2
lizaBukia <123838834+lizaBukia@users.noreply.github.com>
2024-06-13 21:05:28 +0400

    create: button component

---
+++ app/Components/Button/Button.tsx
@@ -0,0 +1,19 @@
+import styles from './Button.module.scss';
+import { ButtonIconPositionEnum } from './enums/button-icon-position.enum';
+import { ButtonPropsInterface } from './interfaces/button-props.interface';
+import { ButtonType } from './types/button.type';
+
+const Button: ButtonType = (props: ButtonPropsInterface) => {
+  return (
+    <button
+      onClick={props.onClick}
+      className={`${styles.button} ${styles[props.type]}`}
+    >
+      {props.position === ButtonIconPositionEnum.Left && props.icon}
+      {props.children}
+      {props.position === ButtonIconPositionEnum.Right && props.icon}
+    </button>
+  );
+};
+
+export default Button;
