8a55c1860b08f35cfabfed6e3809ab806cdeed6d
lizaBukia <123838834+lizaBukia@users.noreply.github.com>
2024-06-19 21:10:54 +0400

    chore: resolved conflict

--- app/Components/Input/interfaces/input-props.interface.ts
+++ app/Components/Input/interfaces/input-props.interface.ts
@@ -3,7 +3,6 @@ import { InputIconPositionEnum } from '../enum/input-icon-position.enum';
 import { InputTypeEnum } from '../enum/input-type.enum';
 
 export interface InputPropsInterface {
-  position: InputIconPositionEnum;
   icon?: ReactNode;
   iconPosition?: InputIconPositionEnum;
   type: InputTypeEnum;
