45a99139b68ba252b266f4a6ab254a3d5a3707a7
Keretcha <163843025+Keretcha@users.noreply.github.com>
2024-06-21 13:20:52 +0400

    chore: resolved comments

--- app/Components/NavMenu/navMenu.tsx
+++ app/Components/NavMenu/navMenu.tsx
@@ -4,25 +4,20 @@ import styles from './navMenu.module.scss';
 import { NavMenuType } from './types/nav-menu.type';
 
 const NavMenu: NavMenuType = (props: { items: NavItemsInerface[] }) => {
-  console.log(props.items);
-
   return (
     <div className={styles.container}>
       <div className={styles.items}>
-        {props.items.length &&
-          props.items.map((item) => {
-            return (
-              <NavItem
-                key={item.href}
-                icon={item.icon}
-                href={item.href}
-                title={item.title}
-              />
-            );
-          })}
+        {props.items.length > 0 &&
+          props.items.map((item) => (
+            <NavItem
+              key={item.href}
+              icon={item.icon}
+              href={item.href}
+              title={item.title}
+            />
+          ))}
       </div>
     </div>
   );
 };
-
 export default NavMenu;
