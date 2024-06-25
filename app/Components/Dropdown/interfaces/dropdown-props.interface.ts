7ca17287d1c97daac9423661c8748886040b4c8d
Gvanca <gvancanando@gmail.com>
2024-06-25 15:18:43 +0400

    chore: fixed comments

--- app/Components/Dropdown/interfaces/dropdown-props.interface.ts
+++ app/Components/Dropdown/interfaces/dropdown-props.interface.ts
@@ -8,3 +8,4 @@ export interface DropdownPropsInterface {
   darkMode: boolean;
   items: DropDownItemsInterface[];
 }
+export { DropDownPositionEnum };
--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,5 @@
-import Dropdown from './Components/Dropdown/Dropdown';
-import { DropDownPositionEnum } from './Components/Dropdown/interfaces/dropdown-props.interface';
-import Icon from './Components/Icon/Icon';
-import { IconNameEnum } from './Components/Icon/enums/icon-name.enum';
 import styles from './page.module.scss';
 
 export default function Home(): JSX.Element {
-  return (
-    <main className={styles.main}>
-      <div>
-        <Dropdown
-          icon={<Icon name={IconNameEnum.Dot} width={20} height={20} />}
-          position={DropDownPositionEnum.Middle}
-          darkMode={false}
-          items={[]}
-        />
-      </div>
-    </main>
-  );
+  return <main className={styles.main}></main>;
 }
