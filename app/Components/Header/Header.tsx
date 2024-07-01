210e205149be67c974d9d04a5677e60919473980
Keretcha <163843025+Keretcha@users.noreply.github.com>
2024-07-01 16:14:01 +0400

    refactor: header component

--- app/Components/Header/Header.tsx
+++ app/Components/Header/Header.tsx
@@ -1,5 +1,5 @@
 'use client';
-import Link from 'next/link';
+
 import { useState } from 'react';
 import { useRecoilState } from 'recoil';
 import Button from '../Button/Button';
@@ -7,12 +7,11 @@ import { ButtonIconPositionEnum } from '../Button/enums/button-icon-position.enu
 import { ButtonTypeEnum } from '../Button/enums/button-type.enum';
 import Icon from '../Icon/Icon';
 import { IconNameEnum } from '../Icon/enums/icon-name.enum';
-import Input from '../Input/Input';
-import { InputIconPositionEnum } from '../Input/enum/input-icon-position.enum';
-import { InputTypeEnum } from '../Input/enum/input-type.enum';
-import NavMenu from '../NavMenu/NavMenu';
 import { isModeState } from '../States/States';
 import styles from './Header.module.scss';
+import HeaderNav from './header-navigation/header-navigation';
+import Logo from './logo/logo';
+import Search from './searchInput/searchInput';
 import { HeaderType } from './types/header.type';
 
 const Header: HeaderType = () => {
@@ -28,27 +27,8 @@ const Header: HeaderType = () => {
   return (
     <div className={mode ? styles.lightContainer : styles.darkContainer}>
       <div className={styles.content}>
-        <Link href={'/'}>
-          <h1 className={styles.logo}>DNCK</h1>
-        </Link>
-        <div className={styles.inputContainer}>
-          <div className={styles.arrow}>
-            <Link href={'/'}>
-              <Icon name={IconNameEnum.ArrowLeft} width={40} height={40} />
-            </Link>
-            <Link href={'/'}>
-              <Icon name={IconNameEnum.ArrowRight} width={40} height={40} />
-            </Link>
-          </div>
-          <div className={styles.searching}>
-            <Input
-              type={InputTypeEnum.Text}
-              placeholder={'Search'}
-              icon={<Icon name={IconNameEnum.Search} width={24} height={24} />}
-              iconPosition={InputIconPositionEnum.Left}
-            />
-          </div>
-        </div>
+        <Logo />
+        <Search />
         <div className={styles.modeAndLog}>
           <div onClick={onClick} className={styles.modes}>
             <Icon name={icon} width={24} height={24} />
@@ -64,33 +44,7 @@ const Header: HeaderType = () => {
           </div>
         </div>
       </div>
-      <div className={styles.navigation}>
-        <NavMenu
-          items={[
-            {
-              icon: IconNameEnum.Home,
-              href: '/',
-              title: 'Home',
-            },
-            {
-              icon: IconNameEnum.Search,
-              href: '/Shuffle',
-              title: 'Search',
-              className: styles.search,
-            },
-            {
-              icon: IconNameEnum.Play,
-              href: '/dot',
-              title: 'Playlist',
-            },
-            {
-              icon: IconNameEnum.Album,
-              href: '/albums',
-              title: 'Albums',
-            },
-          ]}
-        />
-      </div>
+      <HeaderNav />
     </div>
   );
 };
