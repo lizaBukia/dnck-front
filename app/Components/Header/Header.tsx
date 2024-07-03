14b4762e78f07d2026102c976c3f41466f3b655a
Keretcha <163843025+Keretcha@users.noreply.github.com>
2024-07-03 16:18:21 +0400

    refactor: created new components for header

--- app/Components/Header/Header.tsx
+++ app/Components/Header/Header.tsx
@@ -1,28 +1,22 @@
 'use client';
 
-import { useState } from 'react';
 import { useRecoilState } from 'recoil';
-import Button from '../Button/Button';
-import { ButtonIconPositionEnum } from '../Button/enums/button-icon-position.enum';
-import { ButtonTypeEnum } from '../Button/enums/button-type.enum';
-import Icon from '../Icon/Icon';
-import { IconNameEnum } from '../Icon/enums/icon-name.enum';
 import styles from './Header.module.scss';
+import HeaderButton from './HeaderButton/HeaderButton';
 import HeaderNav from './HeaderNavigation/HeaderNavigation';
+import { HeaderPropsInterface } from './Interfaces/Header-props.interface';
 import Logo from './Logo/Logo';
+import Mode from './Mode/Mode';
 import Search from './SearchInput/SearchInput';
 import { HeaderType } from './Type/Header.type';
 import { isModeState } from '@/app/States/States';
 
-const Header: HeaderType = () => {
-  const [icon, setIcon] = useState(IconNameEnum.Sun);
+const Header: HeaderType = (props: HeaderPropsInterface) => {
   const [dark, setDark] = useRecoilState(isModeState);
-  const onClick: () => void = () => {
-    const newIcon: IconNameEnum =
-      icon === IconNameEnum.Sun ? IconNameEnum.Moon : IconNameEnum.Sun;
-    setIcon(newIcon);
-    setDark(!dark);
-  };
+
+  if (props.mode) {
+    setDark(dark);
+  }
 
   return (
     <div className={dark ? styles.lightContainer : styles.darkContainer}>
@@ -30,19 +24,8 @@ const Header: HeaderType = () => {
         <Logo />
         <Search />
         <div className={styles.modeAndLog}>
-          <div onClick={onClick} className={styles.modes}>
-            <Icon name={icon} width={24} height={24} />
-          </div>
-
-          <div className={styles.button}>
-            <Button
-              position={ButtonIconPositionEnum.Right}
-              icon={<Icon name={IconNameEnum.Logout} width={24} height={24} />}
-              type={ButtonTypeEnum.Primary}
-            >
-              Log Out
-            </Button>
-          </div>
+          <Mode />
+          <HeaderButton />
         </div>
       </div>
       <HeaderNav />
@@ -51,3 +34,7 @@ const Header: HeaderType = () => {
 };
 
 export default Header;
+
+// function useRecoilState(isModeState: any): [any, any] {
+//   throw new Error('Function not implemented.');
+// }
---
+++ app/Components/Header/Mode/Mode.tsx
@@ -0,0 +1,29 @@
+'use client';
+
+import { useState } from 'react';
+import { useRecoilState } from 'recoil';
+import Icon from '../../Icon/Icon';
+import { IconNameEnum } from '../../Icon/enums/icon-name.enum';
+import styles from './Mode.module.scss';
+import { isModeState } from '@/app/States/States';
+
+const Mode = (): JSX.Element => {
+  const [icon, setIcon] = useState(IconNameEnum.Sun);
+  const [dark, setDark] = useRecoilState(isModeState);
+  const onClick: () => void = () => {
+    const newIcon: IconNameEnum =
+      icon === IconNameEnum.Sun ? IconNameEnum.Moon : IconNameEnum.Sun;
+    setIcon(newIcon);
+    setDark(!dark);
+  };
+
+  return (
+    <div className={dark ? styles.lightContainer : styles.darkContainer}>
+      <div onClick={onClick} className={styles.modes}>
+        <Icon name={icon} width={24} height={24} />
+      </div>
+    </div>
+  );
+};
+
+export default Mode;
