f8d2a548b1153007009e4145f3edfdb2ed026222
Keretcha <163843025+Keretcha@users.noreply.github.com>
2024-07-04 13:42:29 +0400

    chore: resolved comments

--- app/Components/Header/Mode/Mode.tsx
+++ app/Components/Header/Mode/Mode.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useState } from 'react';
+// import { useState } from 'react';
 import { useRecoilState } from 'recoil';
 import Icon from '../../Icon/Icon';
 import { IconNameEnum } from '../../Icon/enums/icon-name.enum';
@@ -8,19 +8,21 @@ import styles from './Mode.module.scss';
 import { isDarkState } from '@/app/States/States';
 
 const Mode = (): JSX.Element => {
-  const [icon, setIcon] = useState(IconNameEnum.Sun);
   const [dark, setDark] = useRecoilState(isDarkState);
   const onClick: () => void = () => {
-    const newIcon: IconNameEnum =
-      icon === IconNameEnum.Sun ? IconNameEnum.Moon : IconNameEnum.Sun;
-    setIcon(newIcon);
     setDark(!dark);
   };
 
   return (
     <div className={dark ? styles.lightContainer : styles.darkContainer}>
       <div onClick={onClick} className={styles.modes}>
-        <Icon name={icon} width={24} height={24} />
+        <div>
+          <Icon
+            name={dark ? IconNameEnum.Moon : IconNameEnum.Sun}
+            width={24}
+            height={24}
+          />
+        </div>
       </div>
     </div>
   );
