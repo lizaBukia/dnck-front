fc92b745ee01da109daf56603fe136981eef5a1a
Keretcha <163843025+Keretcha@users.noreply.github.com>
2024-07-26 15:33:04 +0400

    refactor: deleted content heading component

--- app/Components/ContentHeading/type/ContentHeading.type.ts
+++
@@ -1,5 +0,0 @@
-import { ContentHeadingPropsInterface } from '../interfaces/content-heading-props.interface';
-
-export type ContentHeadingType = (
-  props: ContentHeadingPropsInterface,
-) => JSX.Element;
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,17 @@
 'use client';
+import Link from 'next/link';
 import { useEffect } from 'react';
 import { useRecoilState } from 'recoil';
 import AlbumCards from './Components/AlbumCards/AlbumCards';
 import AlbumItems from './Components/AlbumItems/AlbumItems';
-import ContentHeading from './Components/ContentHeading/ContentHeading';
+import Heading from './Components/Heading/Heading';
+import { HeadingTypeEnum } from './Components/Heading/enums/heading-type.enum';
 import { HitsItems } from './Components/HitsCard/HitsItems/HitsItems';
 import HitsCards from './Components/HitsCards/HitsCards';
 import Player from './Components/Player/Player';
+import Text from './Components/Text/Text';
+import { TextHtmlTypeEnum } from './Components/Text/enums/text-html-type.enum';
+import { TextTypeEnum } from './Components/Text/enums/text-type.enum';
 import { isDarkState } from './States/States';
 import styles from './page.module.scss';
 
@@ -23,23 +28,63 @@ export default function Home(): JSX.Element {
         <div
           className={`${styles.content} ${dark ? styles.darkContent : styles.lightContent}`}
         >
-          <ContentHeading>Top Albums</ContentHeading>
+          <div className={styles.heading}>
+            <Heading type={HeadingTypeEnum.H5}>Top Albums</Heading>
+            <Text
+              htmlType={TextHtmlTypeEnum.Span}
+              type={TextTypeEnum.SecondaryTextMedium}
+            >
+              See all
+            </Text>
+          </div>
           <AlbumCards items={AlbumItems} />
-
-          <ContentHeading>Top Hits</ContentHeading>
+          <div className={styles.heading}>
+            <Heading type={HeadingTypeEnum.H5}>Top Hits</Heading>
+            <Text
+              htmlType={TextHtmlTypeEnum.Span}
+              type={TextTypeEnum.SecondaryTextMedium}
+            >
+              See all
+            </Text>
+          </div>
 
           <HitsCards items={HitsItems} />
 
-          <ContentHeading>This Week Popular Artists</ContentHeading>
+          <div className={styles.heading}>
+            <Heading type={HeadingTypeEnum.H5}>
+              This Week Popular Artists
+            </Heading>
+            <Text
+              htmlType={TextHtmlTypeEnum.Span}
+              type={TextTypeEnum.SecondaryTextMedium}
+            >
+              See all
+            </Text>
+          </div>
+          <AlbumCards items={AlbumItems} />
+          <div className={styles.heading}>
+            <Heading type={HeadingTypeEnum.H5}>Top Artists</Heading>
+            <Text
+              htmlType={TextHtmlTypeEnum.Span}
+              type={TextTypeEnum.SecondaryTextMedium}
+            >
+              See all
+            </Text>
+          </div>
 
           <AlbumCards items={AlbumItems} />
 
-          <ContentHeading>Top Artists</ContentHeading>
-
-          <AlbumCards items={AlbumItems} />
-
-          <ContentHeading>Top Charts</ContentHeading>
-
+          <div className={styles.heading}>
+            <Heading type={HeadingTypeEnum.H5}>Top Charts</Heading>
+            <Link href={'./home'}>
+              <Text
+                htmlType={TextHtmlTypeEnum.Span}
+                type={TextTypeEnum.SecondaryTextMedium}
+              >
+                See all
+              </Text>
+            </Link>
+          </div>
           <AlbumCards items={AlbumItems} />
         </div>
         <div
