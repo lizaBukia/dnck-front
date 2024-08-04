28e20937a30ccdf0995773d6cdeffdebcd4b54ff
pailodze <mikheilizee@gmail.com>
2024-08-04 22:35:45 +0400

    fix: conflicts

--- app/Components/Dropdown/DropdownButton/DropdownButton.tsx
+++ app/Components/Dropdown/DropdownButton/DropdownButton.tsx
@@ -3,8 +3,8 @@ import { IconNameEnum } from '../../Icon/enums/icon-name.enum';
 import { TextHtmlTypeEnum } from '../../Text/enums/text-html-type.enum';
 import { TextTypeEnum } from '../../Text/enums/text-type.enum';
 import styles from './DropDownButton.module.scss';
-import { DropdownButtonPropsInterface } from './interfaces/dropdown-button-props.interface';
-import { DropdownButtonType } from './types/dropdown-button.type';
+import { DropdownButtonPropsInterface } from '@/app/Components/Dropdown/DropdownButton/interfaces/dropdown-button-props.interface';
+import { DropdownButtonType } from '@/app/Components/Dropdown/DropdownButton/types/dropdown-button.type';
 import Text from '@/app/Components/Text/Text';
 
 const DropdownButton: DropdownButtonType = (
