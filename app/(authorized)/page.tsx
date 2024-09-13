2c4c1a1df130a114484c7cbbb79e2e696fe3f07c
Keretcha <i.keretchashvili97@gmail.com>
2024-09-13 18:10:12 +0400

    fix: fix aws errors

--- app/(authorized)/page.tsx
+++ app/(authorized)/page.tsx
@@ -2,8 +2,8 @@
 import Link from 'next/link';
 import AlbumCards from '../Components/AlbumCards/AlbumCards';
 import AlbumItems from '../Components/AlbumItems/AlbumItems';
-import ArtistCardsItems from '../Components/ArtistCardsItems/ArtistCardsItems';
 import ArtistCards from '../Components/ArtistCards/ArtistCards';
+import ArtistCardsItems from '../Components/ArtistCardsItems/ArtistCardsItems';
 import ModeSwitcher from '../Components/Header/ModeSwitcher/ModeSwitcher';
 import Heading from '../Components/Heading/Heading';
 import { HeadingTypeEnum } from '../Components/Heading/enums/heading-type.enum';
