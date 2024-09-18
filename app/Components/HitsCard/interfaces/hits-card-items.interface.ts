0ab7c4a783c1755097ad6392ca939e0d22798057
lizaBukia <lizi.bukia.dmc21@gipa.ge>
2024-09-18 14:34:39 +0400

    chore: reslve build errors

--- app/Components/HitsCard/interfaces/hits-card-items.interface.ts
+++ app/Components/HitsCard/interfaces/hits-card-items.interface.ts
@@ -1,11 +1,11 @@
 import { DropDownItemsInterface } from '../../Dropdown/interfaces/dropdown-items-props.interface';
 import { PlayButtonPropsInterface } from '../../PlayButton/interfaces/play-button-props.interface';
-import { AlbumInterface } from '@/app/Interfaces/album.interface';
+import { AlbumInterfaces } from '@/app/Interfaces/album.interfaces';
 
 export interface HitsCardItemsInterface {
   dropDownItems?: DropDownItemsInterface[];
   backgroundImage: string;
-  album: AlbumInterface;
+  album: AlbumInterfaces;
   button?: PlayButtonPropsInterface[];
   src: string;
   name: string;
