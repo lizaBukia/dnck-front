6b8abd57511cc1edf693c31942164fbbec1f81d8
Keretcha <i.keretchashvili97@gmail.com>
2024-09-20 15:00:23 +0400

    chore: resolved anything

--- app/(authorized)/artist/[id]/page.tsx
+++ app/(authorized)/artist/[id]/page.tsx
@@ -1,14 +1,11 @@
 'use client';
 import Link from 'next/link';
 import useSWR from 'swr';
-import AlbumCards from '../../Components/AlbumCards/AlbumCards';
-import AlbumItems from '../../Components/AlbumItems/AlbumItems';
-import Heading from '../../Components/Heading/Heading';
-import { HeadingTypeEnum } from '../../Components/Heading/enums/heading-type.enum';
-// import { HitsItems } from '../../Components/HitsCard/HitsItems/HitsItems';
-// import HitsCards from '../../Components/HitsCards/HitsCards';
-import Player from '../../Components/Player/Player';
-import SingleArtistCard from '../../Components/SingleArtistCard/SingleArtistCard';
+import AlbumCards from '../../../Components/AlbumCards/AlbumCards';
+import AlbumItems from '../../../Components/AlbumItems/AlbumItems';
+import Heading from '../../../Components/Heading/Heading';
+import { HeadingTypeEnum } from '../../../Components/Heading/enums/heading-type.enum';
+import SingleArtistCard from '../../../Components/SingleArtistCard/SingleArtistCard';
 import { SingleArtistPagePropsInterface } from '../interfaces/single-artist-page-props.interface';
 import styles from '../page.module.scss';
 import { SingleArtistPageType } from '../type/single-artist-page.type';
@@ -22,7 +19,7 @@ const SingleArtistPage: SingleArtistPageType = (
     `/artists/${props.params.id}`,
     fetcher,
   );
-  console.log(artists);
+
   return (
     <div className={`${styles.mainContainer} ${styles.mainLightContainer}`}>
       <div className={styles.container}>
@@ -30,7 +27,7 @@ const SingleArtistPage: SingleArtistPageType = (
           <div className={styles.singleArtistCard}>
             {artists && (
               <SingleArtistCard
-                artistName={artists.firstName}
+                artistName={`${(artists.firstName, artists.lastName)}`}
                 albums={'hdbshdg'}
               />
             )}
@@ -54,11 +51,6 @@ const SingleArtistPage: SingleArtistPageType = (
           </div>
 
           <AlbumCards items={AlbumItems} />
-        </div>
-        <div
-          className={`${styles.player} ${styles.darkPlayer} ${styles.downPlayer}`}
-        >
-          <Player />
         </div>
       </div>
     </div>
